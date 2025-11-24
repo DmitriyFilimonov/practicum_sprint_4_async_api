fc1a64986354c9e73ebaa08cf25dfd190d49a3a5
Dmitriy Filimonov <johndoe@example.com>
2025-11-24 19:42:17 +0300

    initial project structure

---
+++ src/main.py
@@ -0,0 +1,94 @@
+from http import HTTPStatus
+from http.client import HTTPException
+import logging
+from api.v1 import films
+from core.logger import LOGGING
+from fastapi import APIRouter, Depends, FastAPI
+from fastapi.responses import ORJSONResponse
+from pydantic import BaseModel
+
+from core import config
+from db import elastic
+from db import redis
+from elasticsearch import AsyncElasticsearch
+from redis import Redis
+
+import uvicorn
+
+from services.film import FilmService, get_film_service
+
+router = APIRouter()
+
+
+class Film(BaseModel):
+    id: str
+    title: str
+
+
+# Внедряем FilmService с помощью Depends(get_film_service)
+@router.get("/{film_id}", response_model=Film)
+async def film_details(
+    film_id: str, film_service: FilmService = Depends(get_film_service)
+) -> Film:
+    film = await film_service.get_by_id(film_id)
+    if not film:
+        # Если фильм не найден, отдаём 404 статус
+        # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum    # Такой код будет более поддерживаемым
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="film not found")
+
+    # Перекладываем данные из models.Film в Film
+    # Обратите внимание, что у модели бизнес-логики есть поле description,
+    # которое отсутствует в модели ответа API.
+    # Если бы использовалась общая модель для бизнес-логики и формирования ответов API,
+    # вы бы предоставляли клиентам данные, которые им не нужны
+    # и, возможно, данные, которые опасно возвращать
+    return Film(id=film.id, title=film.title)
+
+
+app = FastAPI(
+    # Конфигурируем название проекта. Оно будет отображаться в документации
+    title=config.PROJECT_NAME,
+    # Адрес документации в красивом интерфейсе
+    docs_url="/api/openapi",
+    # Адрес документации в формате OpenAPI
+    openapi_url="/api/openapi.json",
+    # Можно сразу сделать небольшую оптимизацию сервиса
+    # и заменить стандартный JSON-сериализатор на более шуструю версию, написанную на Rust
+    default_response_class=ORJSONResponse,
+)
+
+
+@app.on_event("startup")
+async def startup():
+    # Подключаемся к базам при старте сервера
+    # Подключиться можем при работающем event-loop
+    # Поэтому логика подключения происходит в асинхронной функции
+    redis.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
+    elastic.es = AsyncElasticsearch(
+        hosts=[f"{config.ELASTIC_SCHEMA}{config.ELASTIC_HOST}:{config.ELASTIC_PORT}"]
+    )
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await redis.redis.close()
+    await elastic.es.close()
+
+
+# Подключаем роутер к серверу, указав префикс /v1/films
+# Теги указываем для удобства навигации по документации
+app.include_router(films.router, prefix="/api/v1/films", tags=["films"])
+
+
+if __name__ == "__main__":
+    # Приложение может запускаться командой
+    # `uvicorn main:app --host 0.0.0.0 --port 8000`
+    # но чтобы не терять возможность использовать дебагер,
+    # запустим uvicorn-сервер через python
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        log_config=LOGGING,
+        log_level=logging.DEBUG,
+    )
