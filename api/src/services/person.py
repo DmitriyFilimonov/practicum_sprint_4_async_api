177e32ad0895bfe8d20310b4c9b96c646581c1ac
Dmitriy Filimonov <johndoe@example.com>
2025-12-31 02:31:50 +0300

    person details cache

--- api/src/services/person.py
+++ api/src/services/person.py
@@ -7,6 +7,8 @@ from elasticsearch import AsyncElasticsearch, NotFoundError
 from src.models.person import Person
 from src.db.elastic import PERSONS_ES_INDEX, get_elastic
 
+PERSON_CACHE_EXPIRE_IN_SECONDS = 60 * 5
+
 
 class PersonService:
     def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
@@ -14,7 +16,14 @@ class PersonService:
         self.elastic = elastic
 
     async def get_person(self, id: str):
+        person = await self._get_person_from_cache(id=id)
+
+        if person:
+            return person
+
         person = await self._get_person_from_elastic(id=id)
+
+        await self._put_person_to_cache(person)
 
         return person
 
@@ -27,6 +36,17 @@ class PersonService:
         except NotFoundError:
             return None
         return Person(**doc["_source"])
+
+    async def _put_person_to_cache(self, person: Person):
+        await self.redis.set(person.id, person.json(), PERSON_CACHE_EXPIRE_IN_SECONDS)
+
+    async def _get_person_from_cache(self, id: str):
+        person = await self.redis.get(id)
+
+        if person:
+            return Person.parse_raw(person)
+
+        return None
 
 
 @lru_cache()
