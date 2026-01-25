893688e7adfed72d9e09d38edcd596437aaa15ab
Dmitriy Filimonov <dmitr1yfilimonoff@yandex.ru>
2026-01-25 22:49:06 +0300

    fix(redis-waiter): 04 os getenv instead of settings, sync redis client

--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,14 +1,16 @@
 import time
+import os
+from redis import Redis
 
-from redis.asyncio import Redis
 
-from tests.functional import settings
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
 
 
 if __name__ == "__main__":
     redis_client = Redis(
-        host=settings.settings.redis_host,
-        port=settings.settings.redis_port,
+        host=REDIS_HOST,
+        port=REDIS_PORT,
     )
     while True:
         if redis_client.ping():
