1967c26e72bd15a3f6ecd194db3c9892e6d6c964
Dmitriy Filimonov <dmitr1yfilimonoff@yandex.ru>
2026-01-31 17:19:24 +0300

    tests(fixtures): 05 es_client fixture

--- tests/functional/src/conftest.py
+++ tests/functional/src/conftest.py
@@ -6,13 +6,19 @@ import pytest_asyncio
 from tests.functional.settings import settings
 
 
+@pytest_asyncio.fixture(name="es_client")
+async def es_client():
+    es_client = AsyncElasticsearch(
+        hosts=f"http://{settings.elastic_host}:{settings.elastic_port}",
+        verify_certs=False,
+    )
+    yield es_client
+    await es_client.close()
+
+
 @pytest_asyncio.fixture(name="es_write_data")
-def es_write_data():
+def es_write_data(es_client: AsyncElasticsearch):
     async def inner(data: list[dict]):
-        es_client = AsyncElasticsearch(
-            hosts=f"http://{settings.elastic_host}:{settings.elastic_port}",
-            verify_certs=False,
-        )
         if await es_client.indices.exists(index=settings.elastic_index):
             await es_client.indices.delete(index=settings.elastic_index)
         await es_client.indices.create(
@@ -20,8 +26,6 @@ def es_write_data():
         )
 
         updated, errors = await async_bulk(client=es_client, actions=data, refresh=True)
-
-        await es_client.close()
 
         if errors:
             raise Exception("Ошибка записи данных в Elasticsearch")
